b255931e17efc5be8742c862d85ca0211fccd7ca
Travis Veralrud <tbveralrud@gmail.com>
2014-03-18 21:47:37 -0700

    Added tests demonstrating failing cases for colorWithHexString.

--- ColorWithHexTests/ColorWithHexTests.m
+++ ColorWithHexTests/ColorWithHexTests.m
@@ -7,6 +7,7 @@
 //
 
 #import "ColorWithHexTests.h"
+#import "AVHexColor.h"
 
 
 @implementation ColorWithHexTests
@@ -25,9 +26,58 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testBlackRGBThreeDigitString
 {
-    STFail(@"Unit tests are not implemented yet in ColorWithHexTests");
+    UIColor *color = [AVHexColor colorWithHexString:@"#000"];
+    STAssertNotNil(color, @"Color should not be nil.");
+
+    CGFloat red, green, blue;
+    STAssertTrue([color getRed:&red green:&green blue:&blue alpha:nil], @"Color components should be readable.");
+
+    STAssertEquals(red, (CGFloat)0, @"Red component should be at lowest intensity.");
+    STAssertEquals(green, (CGFloat)0, @"Green component should be at lowest intensity.");
+    STAssertEquals(blue, (CGFloat)0, @"Blue component should be at lowest intensity.");
+}
+
+- (void)testBlackRGBAFourDigitString
+{
+    UIColor *color = [AVHexColor colorWithHexString:@"#F000"];
+    STAssertNotNil(color, @"Color should not be nil.");
+
+    CGFloat red, green, blue, alpha;
+    STAssertTrue([color getRed:&red green:&green blue:&blue alpha:&alpha], @"Color components should be readable.");
+
+    STAssertEquals(red, (CGFloat)0, @"Red component should be at lowest intensity.");
+    STAssertEquals(green, (CGFloat)0, @"Green component should be at lowest intensity.");
+    STAssertEquals(blue, (CGFloat)0, @"Blue component should be at lowest intensity.");
+    STAssertEquals(alpha, (CGFloat)1, @"Alpha component should be at highest intensity.");
+}
+
+- (void)testBlackRGBSixDigitString
+{
+    UIColor *color = [AVHexColor colorWithHexString:@"#000000"];
+    STAssertNotNil(color, @"Color should not be nil.");
+
+    CGFloat red, green, blue;
+    STAssertTrue([color getRed:&red green:&green blue:&blue alpha:nil], @"Color components should be readable.");
+
+    STAssertEquals(red, (CGFloat)0, @"Red component should be at lowest intensity.");
+    STAssertEquals(green, (CGFloat)0, @"Green component should be at lowest intensity.");
+    STAssertEquals(blue, (CGFloat)0, @"Blue component should be at lowest intensity.");
+}
+
+- (void)testBlackRGBAEightString
+{
+    UIColor *color = [AVHexColor colorWithHexString:@"#FF000000"];
+    STAssertNotNil(color, @"Color should not be nil.");
+
+    CGFloat red, green, blue, alpha;
+    STAssertTrue([color getRed:&red green:&green blue:&blue alpha:&alpha], @"Color components should be readable.");
+
+    STAssertEquals(red, (CGFloat)0, @"Red component should be at lowest intensity.");
+    STAssertEquals(green, (CGFloat)0, @"Green component should be at lowest intensity.");
+    STAssertEquals(blue, (CGFloat)0, @"Blue component should be at lowest intensity.");
+    STAssertEquals(alpha, (CGFloat)1, @"Alpha component should be at highest intensity.");
 }
 
 @end
